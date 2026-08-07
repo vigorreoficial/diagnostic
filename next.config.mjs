a14a387371a0076a7522aa48fa06c32b389cf318
Raulzito <vigorre.oficial@gmail.com>
2026-08-07 15:25:26 -0300

    Create next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['cevlornqbvwshuuoswcj.supabase.co'],
+  },
+}
+
+export default nextConfig
