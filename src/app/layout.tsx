0250e2b5383587453f44b101cf2625bd3f89f11f
Raulzito <vigorre.oficial@gmail.com>
2026-08-07 15:28:03 -0300

    Create layout.tsx

---
+++ src/app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+import { Toaster } from 'sonner'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Vigorre Diagnostics™',
+  description: 'Dados que transformam decisões.',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="pt-BR">
+      <body className={inter.className}>
+        {children}
+        <Toaster position="top-right" richColors />
+      </body>
+    </html>
+  )
+}
