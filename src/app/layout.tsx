c742b501c78ceb0ca172d161cd8e3168e6e81ed9
Raulzito <vigorre.oficial@gmail.com>
2026-08-10 19:28:19 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,9 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'Vigorre Diagnostics™',
   description: 'Dados que transformam decisões.',
+  icons: {
+    icon: '/favicon.ico',
+  },
 }
 
 export default function RootLayout({
