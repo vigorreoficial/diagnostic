74ffa868ea1c9ce9add6d6f91d5821b7874e5edd
Raulzito <vigorre.oficial@gmail.com>
2026-08-10 12:39:40 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,6 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
 import { Toaster } from 'sonner'
-
-const inter = Inter({ subsets: ['latin'] })
+import './globals.css'
 
 export const metadata: Metadata = {
   title: 'Vigorre Diagnostics™',
@@ -12,14 +9,14 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode
-}) {
+}>) {
   return (
     <html lang="pt-BR">
-      <body className={inter.className}>
+      <body>
         {children}
-        <Toaster position="top-right" richColors />
+        <Toaster richColors position="top-right" />
       </body>
     </html>
   )
