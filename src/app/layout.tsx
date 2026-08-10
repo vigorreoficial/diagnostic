dc15b519bc638e963ea8a66f4438ab06f4178593
Raulzito <vigorre.oficial@gmail.com>
2026-08-10 17:17:24 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { Toaster } from 'sonner'
-import { ThemeProvider } from '@/providers/ThemeProvider'
+import { ThemeProvider } from '@/app/providers/ThemeProvider'
 
 const inter = Inter({ subsets: ['latin'] })
 
