5ea18d16843e0076103226b8d2d40a31b19c43c9
Raulzito <vigorre.oficial@gmail.com>
2026-08-11 11:18:23 -0300

    Update page.tsx

--- src/app/(dashboard)/diagnosticos/[id]/relatorios/page.tsx
+++ src/app/(dashboard)/diagnosticos/[id]/relatorios/page.tsx
@@ -79,7 +79,7 @@ export default function GerenciarRelatoriosPage() {
           const { data } = await supabase
             .from('usuarios')
             .select('*')
-            .eq('auth_user_id', user.id)
+            .eq('user_id', user.id)
             .single()
           setUserData(data)
         }
