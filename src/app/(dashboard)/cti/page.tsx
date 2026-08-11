4d9182a42750802509f16256a2472c9c24d8ec1e
Raulzito <vigorre.oficial@gmail.com>
2026-08-11 11:30:05 -0300

    Update page.tsx

--- src/app/(dashboard)/cti/page.tsx
+++ src/app/(dashboard)/cti/page.tsx
@@ -88,7 +88,7 @@ export default function CTIPage() {
           const { data } = await supabase
             .from('usuarios')
             .select('*')
-            .eq('auth_user_id', user.id)
+            .eq('user_id', user.id)
             .single()
           setUserData(data)
         }
