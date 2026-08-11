d31abcfa5d779016848d15a2567d19b0813c3944
Raulzito <vigorre.oficial@gmail.com>
2026-08-11 11:23:45 -0300

    Update page.tsx

--- src/app/(dashboard)/clientes/[id]/page.tsx
+++ src/app/(dashboard)/clientes/[id]/page.tsx
@@ -39,7 +39,7 @@ export default function DetalhesClientePage() {
           const { data } = await supabase
             .from('usuarios')
             .select('*')
-            .eq('auth_user_id', user.id)
+            .eq('user_id', user.id)
             .single()
           setUserData(data)
         }
